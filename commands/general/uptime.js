b282aefa13e55e79889510dcaddbd79fdf3aa6f0
aqua44 <97438711+aqua44@users.noreply.github.com>
2022-08-24 21:56:34 +0300

    Create uptime.js

---
+++ commands/general/uptime.js
@@ -0,0 +1,21 @@
+const { Client, MessageEmbed } = require('discord.js');
+const pretty = require('pretty-ms');
+
+module.exports = {
+    name: 'uptime',
+    description: "Returns information about Exxyll's Uptime",
+   
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        const embed = new MessageEmbed()
+            .setTitle(`🕘 Maily's Uptime`)
+            .setDescription(`\`\`\`yml\nStatus : Online\nUptime : ${pretty(client.uptime)}\n\`\`\``)
+            .setColor("#800080");
+
+        interaction.followUp({ embeds: [embed] });
+    },
+};
