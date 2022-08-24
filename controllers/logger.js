54ae8725f6f260d68a5ccf06a613d74eb58bb405
aqua44 <97438711+aqua44@users.noreply.github.com>
2022-08-24 21:33:04 +0300

    Add files via upload

--- controllers/logger.js
+++ controllers/logger.js
@@ -1,1 +1,66 @@
+//===========================================================\\
+//===================| Coded By Uo#1428 |====================\\
+//==================| https://uo1428.tk/ |===================\\
+//===========================================================\\
+//=============| ΛLL IN ONΞ™ | Development </> |=============\\
+//=============| https://discord.gg/pXRT2FusPb |=============\\
+//===========================================================\\ 
+const chalk = require('chalk');
 
+/**
+ * 
+ * @param {String} message 
+ * @returns {void}
+ */
+function debug(message) {
+	const date = new Date();
+	const dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+	console.log(chalk.blue(`[${dateString}] [DEBUG] ${message}`));
+}
+
+/**
+ * 
+ * @param {String} message 
+ * @returns {void}
+ */
+function warn(message) {
+	const date = new Date();
+	const dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+	console.log(chalk.yellow(`[${dateString}] [WARN] ${message}`));
+}
+
+/**
+ * 
+ * @param {String} message 
+ * @returns {void}
+ */
+function error(message) {
+	const date = new Date();
+	const dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+	console.log(chalk.red(`[${dateString}] [ERROR] ${message}`));
+}
+
+/**
+ * 
+ * @param {String} message 
+ * @returns {void}
+ */
+function success(message) {
+	const date = new Date();
+	const dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+	console.log(chalk.green(`[${dateString}] [SUCCESS] ${message}`));
+}
+
+module.exports = {
+	debug,
+	warn,
+	error,
+	success
+}
+//===========================================================\\
+//===================| Coded By Uo#1428 |====================\\
+//==================| https://uo1428.tk/ |===================\\
+//===========================================================\\
+//=============| ΛLL IN ONΞ™ | Development </> |=============\\
+//=============| https://discord.gg/pXRT2FusPb |=============\\
+//===========================================================\\ 
