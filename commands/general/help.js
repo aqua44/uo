e3e50dadb7ac53206886df6e92c575c000728e7d
aqua44 <97438711+aqua44@users.noreply.github.com>
2022-08-24 21:46:46 +0300

    Create help.js

---
+++ commands/general/help.js
@@ -0,0 +1,34 @@
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+const moment = require("moment");
+
+module.exports = {
+      name: "help",
+      description: "Return all commands!",
+      
+     run: async (client, interaction, args) => {
+    
+
+    let embed = new MessageEmbed()
+    .setTitle(`Help`)  
+    .setColor("#2f3136")
+    .addField("Information Commands", `help, whois, invite, support, botinfo`)
+    .addField("Config Commands", `roles, set, unset, ticket-panel`)
+    .addField(":tickets: Ticket Commands", `add, close, delete, open, remove, reopen, transcript`)
+
+    const row = new MessageActionRow()
+        .addComponents(
+         new MessageButton()
+         .setLabel("Invite")
+         .setStyle("LINK")
+         .setURL(`https://discord.com/oauth2/authorize?client_id=1009839129862869062&permissions=8&scope=bot%20applications.commands`)
+        )
+        .addComponents(
+          new MessageButton()
+          .setLabel("Support")
+          .setStyle("LINK")
+          .setURL(`https://discord.gg/UybeVq66hF`)
+         )
+
+    interaction.followUp({ embeds: [embed], components: [row] });
+  }
+};
