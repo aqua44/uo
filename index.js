3a5b0339eea66ac697066a1d56e811fb2c669d27
aqua44 <97438711+aqua44@users.noreply.github.com>
2022-08-26 11:00:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.listen(port, () => debug(`Bot running on http://localhost:${port}`));
 })
 
 //===========================================================\\
-const pollSchema = require("../module/poll.js");
+const pollSchema = require("../../module/poll");
 const {
     ChartJSNodeCanvas,
     ChartConfiguration,
